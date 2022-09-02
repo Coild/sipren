c8fddef2845fcdfb389ae9cd3cb6f37766d89830
Coild <xreza75@gmail.com>
2022-09-02 23:54:27 +0800

    starter

---
+++ app/controllers/Home.php
@@ -0,0 +1,18 @@
+<?php
+
+class Home extends Controller
+{
+  public function index()
+  {
+    // $data['judul'] = 'Home';
+    // $data['nama'] = $this->model('User_model')->getUser();
+
+    
+    $this->view('templates/app');
+    
+    $this->view('templates/navbar');
+    $this->view('templates/header');
+    $this->view('home/index');
+    $this->view('templates/footer');
+  }
+}
---
+++ app/views/templates/navbar.php
@@ -0,0 +1,14 @@
+<nav class="navbar-default navbar-static-side" role="navigation">
+            <div class="sidebar-collapse">
+                <ul class="nav metismenu" id="side-menu">
+                    <li class="nav-header">
+                            IN+
+                    </li>
+
+                    <li>
+                        <a href="layouts.html"><i class="fa fa-diamond"></i> <span class="nav-label">Layouts</span></a>
+                    </li>
+                </ul>
+
+            </div>
+        </nav>
